a9f2716d1f2a57c9ea571d9914d3bf3fcc7fab8d
Justin <justinas.zaliaduonis@gmail.com>
2021-08-05 13:49:50 +0200

    Initial infrastructure setup

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const API_KEY = process.env.ALCHEMY_API_KEY;
+
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${API_KEY}`,
+      }
+    }
+  }
+};
