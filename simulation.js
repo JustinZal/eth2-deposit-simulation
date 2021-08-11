f877443141bff865cc196c074392cbd317d0deb9
Justin <justinas.zaliaduonis@gmail.com>
2021-08-11 21:13:29 +0200

    Add first working validation version with logging (remove later)

--- simulation.js
+++ simulation.js
@@ -2,10 +2,12 @@ const ethers = require('ethers');
 const abi = require('./lib/deposit.json');
 const bls = require("@chainsafe/bls");
 const blsKeygen =  require("@chainsafe/bls-keygen");
+const { buildMessageRoot } = require('./lib/ssz');
+const { verifySignature, computeSigningRoot, getDomain } = require('./lib/helper');
 
 const DEPOSIT_ADDRESS = '0x00000000219ab540356cbb839cbe05303d7705fa';
 const TRANSFER_AMOUNT = ethers.utils.parseUnits('32', 'ether');
-const DEPOSIT_AMOUNT = ethers.utils.parseUnits('32', 'gwei');
+const DEPOSIT_AMOUNT = Number("32000000000");
 const DEPOSIT_AMOUNT_LE_HEX = '0x0040597307000000'; //THIS is how value is hashed inside the contract
 const PROVIDER_URL = 'http://127.0.0.1:8545';
 
@@ -24,24 +26,14 @@ const generateValidatorKeys = () => {
 const getWithdrawalCredentials = withdrawalKeys =>
     sha256(['bytes'], [withdrawalKeys.publicKey])
 
-const signDepositObject = (keys, withdrawalCredentials) => {
-    const message = ethers.utils.solidityPack(
-        ['bytes', 'bytes', 'uint'],
-        [
-            keys.publicKey,
-            withdrawalCredentials,
-            DEPOSIT_AMOUNT
-        ]
-    )
-
-    return bls.sign(Uint8Array.from(keys.secretKey), message);
-}
+const getDepositObjectHash = (keys, withdrawalCredentials) =>
+    ethers.utils.solidityPack(['bytes', 'bytes', 'uint'], [keys.publicKey, withdrawalCredentials, DEPOSIT_AMOUNT])
 
 const computeDepositDataRoot = (publicKey, signature, withdrawalCredentials) => {
     const publicKeyRoot = sha256(['bytes', 'bytes16'], [publicKey, ethers.constants.HashZero.substring(0, 34)]);
 
-    const firstSignatureHash = sha256(['bytes'], [signature.slice(0, 64)]);
-    const secondSignatureHash = sha256(['bytes', 'bytes32'], [signature.slice(64), ethers.constants.HashZero]);
+    const firstSignatureHash = sha256(['bytes'], [signature.subarray(0, 64)]);
+    const secondSignatureHash = sha256(['bytes', 'bytes32'], [signature.subarray(64), ethers.constants.HashZero]);
     const signatureRoot = sha256(['bytes', 'bytes'], [firstSignatureHash, secondSignatureHash]);
 
     const withdrawalCredentialsHash = sha256(['bytes32', 'bytes'], [publicKeyRoot, withdrawalCredentials]);
@@ -54,25 +46,59 @@ const computeDepositDataRoot = (publicKey, signature, withdrawalCredentials) => 
 }
 
 const simulate = async () => {
+    console.log('Initializing BLS signing library')
     await bls.init()
     const sender = await provider.getSigner();
     const depositContract = new ethers.Contract(DEPOSIT_ADDRESS, abi, provider);
 
+    console.log('Ethereum deposit contract located at:', depositContract.address);
+
     const signingKey = generateValidatorKeys();
     const withdrawalKey = generateValidatorKeys();
-
     const withdrawalCredentials = getWithdrawalCredentials(withdrawalKey);
-    const signature = signDepositObject(signingKey, withdrawalCredentials);
-    const root = computeDepositDataRoot(signingKey.publicKey, signature, withdrawalCredentials)
 
+    console.log('Withdrawal credentials:', withdrawalCredentials)
+
+    //Also root
+    const depositMessageHash = buildMessageRoot({
+        pubkey: Buffer.from(signingKey.publicKey).toString('hex'),
+        withdrawal_credentials: withdrawalCredentials.slice(2),
+        amount: DEPOSIT_AMOUNT
+    });
+
+    console.log('Deposit message hash:', depositMessageHash.toString('hex'));
+
+    const domain = getDomain();
+    console.log('Domain:', Buffer.from(domain).toString('hex'));
+
+    const signingRoot = computeSigningRoot(depositMessageHash, domain);
+
+    const signature = bls.sign(Uint8Array.from(signingKey.secretKey), signingRoot);
+    console.log('Signature:', Buffer.from(signature).toString('hex'));
+
+    const valid = verifySignature({
+        pubkey: Buffer.from(signingKey.publicKey),
+        signature: signature,
+        deposit_message_root: Buffer.from(depositMessageHash, 'hex'),
+    }, bls.verify);
+
+    console.log('Deposit data is valid:', valid);
+    const rootToDeposit = computeDepositDataRoot(
+        Buffer.from(signingKey.publicKey),
+        Buffer.from(signature),
+        Buffer.from(withdrawalCredentials.slice(2), 'hex')
+    );
     const transaction = await depositContract.connect(sender).deposit(
         signingKey.publicKey,
         withdrawalCredentials,
         signature,
-        root,
+        rootToDeposit,
         { value: TRANSFER_AMOUNT }
-    );
+    )
     await transaction.wait();
+
+    console.log('Transaction complete');
+    console.log('Transaction Data:', transaction.data);
 };
 
 simulate();
